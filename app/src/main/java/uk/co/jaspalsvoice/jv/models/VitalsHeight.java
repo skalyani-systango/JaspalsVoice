99fe3bce666582fae2638a40f3071b42b7553498
Srinivas Kalyani <skalyani@isystango.com>
2016-07-14 14:47:50 +0530

    Updated recyclerview library, added other section to vitals

---
+++ app/src/main/java/uk/co/jaspalsvoice/jv/models/VitalsHeight.java
@@ -0,0 +1,57 @@
+package uk.co.jaspalsvoice.jv.models;
+
+import android.content.ContentValues;
+
+import uk.co.jaspalsvoice.jv.db.DbOpenHelper;
+
+/**
+ * Created by Srinivas Kalyani on 11 Jul 16.
+ */
+public class VitalsHeight {
+
+    public String height;
+    public String date;
+    public String uuid;
+    public int id;
+
+    public void setHeight(String height){
+        this.height = height;
+    }
+
+    public void setDate (String date){
+        this.date = date;
+    }
+
+    public String getHeight(){
+        return height;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setUuid(String uuid){
+        this.uuid= uuid;
+    }
+
+    public String getUuid(){
+        return uuid;
+    }
+
+    public void setId(int id){
+        this.id= id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public ContentValues toContentValues() {
+        ContentValues cv = new ContentValues();
+       /* cv.put(DbOpenHelper.COLUMN_B_UUID, getUuid());
+        cv.put(DbOpenHelper.COLUMN_B_ID, getId());*/
+        cv.put(DbOpenHelper.COLUMN_H_HEIGHT, getHeight());
+        cv.put(DbOpenHelper.COLUMN_H_DATE, getDate());
+        return cv;
+    }
+}
---
+++ app/src/main/java/uk/co/jaspalsvoice/jv/models/VitalsWeight.java
@@ -0,0 +1,57 @@
+package uk.co.jaspalsvoice.jv.models;
+
+import android.content.ContentValues;
+
+import uk.co.jaspalsvoice.jv.db.DbOpenHelper;
+
+/**
+ * Created by Srinivas Kalyani on 11 Jul 16.
+ */
+public class VitalsWeight {
+
+    public String weight;
+    public String date;
+    public String uuid;
+    public int id;
+
+    public void setWeight(String weight){
+        this.weight = weight;
+    }
+
+    public void setDate (String date){
+        this.date = date;
+    }
+
+    public String getWeight(){
+        return weight;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setUuid(String uuid){
+        this.uuid= uuid;
+    }
+
+    public String getUuid(){
+        return uuid;
+    }
+
+    public void setId(int id){
+        this.id= id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public ContentValues toContentValues() {
+        ContentValues cv = new ContentValues();
+       /* cv.put(DbOpenHelper.COLUMN_B_UUID, getUuid());
+        cv.put(DbOpenHelper.COLUMN_B_ID, getId());*/
+        cv.put(DbOpenHelper.COLUMN_W_WEIGHT, getWeight());
+        cv.put(DbOpenHelper.COLUMN_W_DATE, getDate());
+        return cv;
+    }
+}
